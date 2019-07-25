566529748c0fc7cc5b299fa5e109ef9da0a87dbe
sajrus33 <brianwala22@gmail.com>
2019-07-26 00:26:48 +0200

    Updates

---
+++ precache-manifest.aa1c21c4da4c04a7f04c549f409b35b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0da5618fc95d5871b3e0f1ab7752d0",
+    "url": "/App-air-pollution/index.html"
+  },
+  {
+    "revision": "121171eae7ea01a85f73",
+    "url": "/App-air-pollution/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "50e51743922de0c625a9",
+    "url": "/App-air-pollution/static/js/2.3974ee62.chunk.js"
+  },
+  {
+    "revision": "121171eae7ea01a85f73",
+    "url": "/App-air-pollution/static/js/main.fd9ec978.chunk.js"
+  },
+  {
+    "revision": "134585519727cd648c7e",
+    "url": "/App-air-pollution/static/js/runtime~main.6134837d.js"
+  }
+]);
