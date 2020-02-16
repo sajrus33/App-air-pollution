d18e3328a77f4b6b63a23a7d31f7ea76dad475ee
Brian <brianwala22@gmail.com>
2020-02-16 18:30:09 +0100

    Updates

---
+++ precache-manifest.46db9b5fcf81ec3caa52707d779e16ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837d524b659c18ce7f4d09bee71836a7",
+    "url": "/App-air-pollution/index.html"
+  },
+  {
+    "revision": "6c87d79d25fcf6dae6ec",
+    "url": "/App-air-pollution/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "aab3037904f2b5216371",
+    "url": "/App-air-pollution/static/js/2.3043fa82.chunk.js"
+  },
+  {
+    "revision": "3e7d956d5814d78462cf6ea8af352468",
+    "url": "/App-air-pollution/static/js/2.3043fa82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c87d79d25fcf6dae6ec",
+    "url": "/App-air-pollution/static/js/main.846d1ff2.chunk.js"
+  },
+  {
+    "revision": "e4bd4efdf523b3e8c634",
+    "url": "/App-air-pollution/static/js/runtime-main.983af4fe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/App-air-pollution/precache-manifest.dcf21b886846395c54ca5215d8d7096d.js"
+  "/App-air-pollution/precache-manifest.46db9b5fcf81ec3caa52707d779e16ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/App-air-pollution/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
